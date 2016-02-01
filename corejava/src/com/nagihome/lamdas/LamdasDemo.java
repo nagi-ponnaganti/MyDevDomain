4ffffa74974c0a82f4e8dcd7d2779d80a53acc27
Nagi Ponnaganti <nagi.ponnaganti@gmail.com>
2016-02-01 23:01:57 +0000

    Starting Lamdas

---
+++ corejava/src/com/nagihome/lamdas/LamdasDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.nagihome.lamdas;
+
+import java.util.Arrays;
+
+/**
+ * @author nageswararao
+ *
+ */
+public class LamdasDemo {
+	
+	public static void main(String[] args) {
+		
+		Arrays.asList("a", "b", "c");
+		
+	}
+
+}
