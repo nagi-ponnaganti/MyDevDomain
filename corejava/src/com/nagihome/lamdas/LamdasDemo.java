2dd7c00e0e1f92193c8b80b4fb9774f0edb56289
Nagi Ponnaganti <nagi.ponnaganti@gmail.com>
2016-02-03 23:18:18 +0000

    Java 8 Commits

--- corejava/src/com/nagihome/lamdas/LamdasDemo.java
+++ corejava/src/com/nagihome/lamdas/LamdasDemo.java
@@ -3,18 +3,39 @@
  */
 package com.nagihome.lamdas;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * @author nageswararao
  *
  */
 public class LamdasDemo {
-	
+
 	public static void main(String[] args) {
-		
-		Arrays.asList("a", "b", "c");
-		
+
+		Consumer<String> consumerTest = LamdasDemo::printNames;
+
+		consumerTest.accept("Nagi Ponnaganti");
+		consumerTest.accept("Monika Gutta");
+
+		List<String> names = new ArrayList<String>();
+		names.add("Nagi");
+		names.add("Moni");
+
+		names.stream().forEach((x) -> {
+			printNames(() -> x);
+		});
+	}
+
+	private static void printNames(String name) {
+		System.out.println(String.format("The Name is: %s ", name));
+	}
+
+	private static void printNames(Supplier<String> arg) {
+		System.out.println("The Short Name is: " + arg.get());
 	}
 
 }
