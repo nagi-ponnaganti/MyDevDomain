90553e53679996870d8892c102f805d39539cc05
Nagi Ponnaganti <nagi.ponnaganti@gmail.com>
2016-04-08 19:12:42 +0100

    Committing unknow, Back to Normal

---
+++ corejava/src/com/nagihome/reflex/Customer.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.nagihome.reflex;
+
+/**
+ * @author nageswararao
+ *
+ */
+public class Customer {
+	
+	private int customerId;
+	private String customerName;
+	private String customerAddress;
+	private String postalCode;
+	
+	/**
+	 * @return the customerId
+	 */
+	public int getCustomerId() {
+		return customerId;
+	}
+	/**
+	 * @param customerId the customerId to set
+	 */
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	/**
+	 * @return the customerName
+	 */
+	public String getCustomerName() {
+		return customerName;
+	}
+	/**
+	 * @param customerName the customerName to set
+	 */
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	/**
+	 * @return the customerAddress
+	 */
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+	/**
+	 * @param customerAddress the customerAddress to set
+	 */
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+	/**
+	 * @return the postalCode
+	 */
+	public String getPostalCode() {
+		return postalCode;
+	}
+	/**
+	 * @param postalCode the postalCode to set
+	 */
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+
+}
