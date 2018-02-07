38812cbf0e92b3ed08e1ad68512427fa057bd121
Nagi Ponnaganti <nagi.ponnaganti@gmail.com>
2018-02-07 20:39:47 +0000

    hibernate chapter 01 with tests

---
+++ samplewebapp/src/main/java/com/nagihome/samplewebapp/temp/SessionUtil.java
@@ -0,0 +1,30 @@
+package com.nagihome.samplewebapp.temp;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.jboss.logging.Logger;
+
+public class SessionUtil {
+    private static final SessionUtil instance = new SessionUtil();
+    private final SessionFactory factory;
+
+    Logger logger = Logger.getLogger(this.getClass());
+
+    private SessionUtil() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    public static Session getSession() {
+        return getInstance().factory.openSession();
+    }
+
+    private static SessionUtil getInstance() {
+        return instance;
+    }
+}
