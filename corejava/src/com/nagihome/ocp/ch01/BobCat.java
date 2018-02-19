889392882a56648442cd2097fc66fc2e7bdbaeef
Nagi Ponnaganti <nagi.ponnaganti@gmail.com>
2018-02-19 14:29:11 +0000

    Overload, Override and InstanceOf

---
+++ corejava/src/com/nagihome/ocp/ch01/BobCat.java
@@ -0,0 +1,10 @@
+package com.nagihome.ocp.ch01;
+
+public class BobCat {
+
+    public Number findDen() throws Exception {
+        System.out.println("Bob Cat Instance");
+        return new Integer(2);
+    }
+
+}
