3ad11cc7b1be1dd93a47042ff6e66a26e57909fe
Nagi Ponnaganti <nagi.ponnaganti@gmail.com>
2019-02-13 16:00:36 +0000

    cleanup

--- sakilavaadin/src/main/java/com/sakila/book/ch02/timeit/biz/Test.java
+++
@@ -1,9 +0,0 @@
-package com.sakila.book.ch02.timeit.biz;
-
-public interface Test {
-
-    void execute();
-
-    String getName();
-
-}
--- sakilavaadin/src/main/java/com/sakila/service/ActorService.java
+++
@@ -1,9 +0,0 @@
-package com.sakila.service;
-
-import com.sakila.domain.Actor;
-
-import java.util.List;
-
-public interface ActorService {
-    public List<Actor> getAllActors();
-}
